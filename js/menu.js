5b40c0f8be8864c4d7c5a781480442e3ba832b82
jah rik <jahrik@hotmail.com>
2020-08-08 14:36:24 -0400

    fixed font size mobile

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,37 @@
-function openSideMenu(){
+/*function openSideMenu(){
     document.getElementById('side-menu').style.width = '250px';
    
 }
 function closeSideMenu(){
    document.getElementById('side-menu').style.width = '0';
    event.preventDefault();
+}*/
+
+let showMenu = false;
+let navicon = document.querySelector('.nav-icon');
+const close = document.querySelector('.btn-close');
+
+   
+
+
+navicon.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+      document.getElementById('side-menu').style.width = '250px';
+      
+        //Set Menu state
+        showMenu = true;
+    }else{
+      document.getElementById('side-menu').style.width = '0px';
+      
+        //Set Menu State
+        showMenu = false;
+
+    }
 }
 
-$('#my-text').change(function() {
-    $('#bag-btn').data('item-custom1-value', $(this).val());
+close.addEventListener("click", () => {
+  document.getElementById('side-menu').style.width = '0px';
+  
 });
